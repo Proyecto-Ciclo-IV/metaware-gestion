486216e15f386f974bd78b7982bf5b1795808cfe
Cabromacabro <oscarpedraza1@hotmail.com>
2021-11-03 21:53:10 -0500

    Primeros ajustes con next.js

---
+++ pages/admin/usuarios/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const index = () => {
+    return (
+        <div>
+            Página Admin/usuarios
+        </div>
+    )
+}
+
+export default index
---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const test = () => {
+    return (
+        <div>
+            Hola, soy Test
+        </div>
+    )
+}
+
+export default test;
